05f1fc1c076533cff169fb3a34b0bd72e0b427c4
Cvetina <cveti_mateeva@abv.bg>
2016-12-02 13:02:12 +0200

    js json worked - now need to put dinamic id for button expand pannel

--- betting_layout/js/functions.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("touchstart", function(){}, true);
-     $(document).on('click', '.navbar-collapse.in', function (e) {
-    if ($(e.target).is('a')) {
-        $(this).collapse('hide');
-    }
-    });
